9f7ac37420464968e97a3d692697c0607abe4e81
Anton <anton@adc.sh>
2019-12-17 22:09:38 +0300

    footer

--- .documentary/index.jsx
+++ .documentary/index.jsx
@@ -3,9 +3,9 @@
  */
 export const footer = () => {
   const alt = 'idiocc'
-  const src = 'https://avatars3.githubusercontent.com/u/38815725?v=4&s=100'
-  const href = 'https://www.artd.eco'
-  const org = 'Art Deco™'
+  const src = 'https://avatars2.githubusercontent.com/u/40834161?s=100&v=4'
+  const href = 'https://www.idio.cc'
+  const org = 'Idio'
   const year = new Date().getFullYear()
   return [
     (<table>
